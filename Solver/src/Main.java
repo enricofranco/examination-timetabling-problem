f06418895726b135aee7b28988c79a3213dca30f
Enrico Franco <francoenrico95@gmail.com>
2017-11-24 11:23:04 +0100

    Relative path solved

--- Solver/src/Main.java
+++ Solver/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Handler h = new Handler();
-		h.loadInstance(".\\src\\demo\\test");
+		h.loadInstance("./src/demo/test");
 		h.initialize();
 	}
 }
