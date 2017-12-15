f2441b26623430146305763e0e6085d1847e6167
Vooriden <emanuele.gullino@mail.com>
2017-12-15 09:25:18 +0100

    Altre prove con i bucket

--- Solver/src/it/polito/oma/solver/threads/Generator.java
+++ Solver/src/it/polito/oma/solver/threads/Generator.java
@@ -1,26 +1,28 @@
 package it.polito.oma.solver.threads;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.TreeMap;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-
-import it.polito.oma.solver.*;
 
 public class Generator implements Runnable  {
 
 	private int T;
 	private int E;
+	private int totExm;
 	private int[][] conflicts;
-	private int[] solution;
 	private int[] isSelected;
-	private int[][] examPriority;
 	private Random rand = new Random();
+
+	List<ArrayList<Integer>> buckets;
+	List<ArrayList<Integer>> timeslots;
+
+	private int[] solution;
 
 	/*
 	 * Set variables into the random generator
@@ -28,125 +30,203 @@ public class Generator implements Runnable  {
 	public Generator(int T, int E, int[][] conflicts) {
 		this.T = T;
 		this.E = E;
+		this.totExm = E;
 		this.conflicts = conflicts;
-		solution = new int[E];
-		rand.setSeed(System.nanoTime());
-		isSelected = new int[E];
-		examPriority = new int[E][2];
-		for(int i = 0; i < E; i++) {
-			examPriority[i][0] = Integer.MAX_VALUE;
-		}
-		int priority;
-		for(int i = 0; i < E; i++) {
-			priority = 0;
-			for(int j = 0; j < E; j++) {
-				if(conflicts[j][i] > 0 || conflicts[i][j] > 0)
-					priority ++;
-			}
-//			System.out.println(i + " " + priority);
-			for(int j = 0; j < E; j++) {
-				if(examPriority[j][0] > priority) {
-					for(int k = E-1; k > j; k--) {
-						examPriority[k][0] = examPriority[k-1][0];
-						examPriority[k][1] = examPriority[k-1][1];
-					}
-					examPriority[j][0] = priority;
-					examPriority[j][1] = i;
-					break;
-				}
-			}
-		}
-		System.out.println();
-		for(int i = 0; i < E; i++) {
-			System.out.print(examPriority[i][0] + " ");
-			System.out.println(examPriority[i][1] + " ");
-		}
-		System.out.println();
+		this.timeslots = new ArrayList<>();
+		this.solution = new int[E];
+		this.rand.setSeed(System.nanoTime());
+		this.isSelected = new int[E];
+		
 	}
 	
 	@Override
 	public void run() {
-		Map<Integer, ArrayList<Integer>> buckets = setBuckets();
-		for(ArrayList<Integer> a:buckets.values()) {
+		buckets = setBuckets();
+//		for(ArrayList<Integer> a:buckets) {
+//			for(Integer I:a) {
+//				System.out.print(I + " ");
+//			}
+//			System.out.println();
+//		}
+//		
+//		System.out.println(buckets.size());
+//		
+//		System.out.println(buckets.get(0).size());
+		
+		try {
+			writeLog();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		ArrayList<Integer> l;
+		
+		for(int i = 0; i < T; i++) {
+			l = new ArrayList<>();
+			timeslots.add(l);
+		}
+		solver(0, 0, buckets.get(0).size());
+		solver(0, buckets.get(0).size(), T);
+		for(ArrayList<Integer> a:timeslots) {
 			for(Integer I:a) {
 				System.out.print(I + " ");
 			}
 			System.out.println();
 		}
-		System.out.println(buckets.keySet().size());
+		
+		System.out.println(totExm);
 	}
 	
-	private Map<Integer, ArrayList<Integer>> setBuckets() {
+	private List<ArrayList<Integer>> setBuckets() {
 		Map<Integer, ArrayList<Integer>> bucket = new TreeMap<>();
-		int iExm, jExm;
-		for(int i = 0; i < E; i++) {
-			iExm = examPriority[i][1];
-			if(isSelected[iExm] != 1) {
-				isSelected[iExm] = 1;
-				ArrayList<Integer> l = new ArrayList<>();
-				l.add(iExm);
-				for(int j = E-1; j >= 0; j--) {
-					jExm = examPriority[j][1];
-					if(isSelected[jExm] != 1) {
-						if(conflicts[jExm][iExm] == 0 && conflicts[iExm][jExm] == 0 && jExm != iExm) {
-							isSelected[jExm] = 1;
-							l.add(jExm);
-							for(int k = 0; k < l.size(); k++) {
-								int exm = l.get(k);
-								if(conflicts[jExm][exm] > 0 || conflicts[exm][jExm] > 0) {
-									l.remove(k);
-									isSelected[exm] = 0;
-								}
-							}
-						}
-						
-					}
-				}
-				bucket.put(i, l);
-			}
-		}
-		List<Integer> reverseKeyset = bucket.keySet()
-				.stream()
-				.sorted(Collections.reverseOrder())
-				.collect(Collectors.toList());
 		ArrayList<Integer> list;
-		ArrayList<Integer> revList;
-		boolean insert = false;
-		for(int l = 0; l < 10; l++) {
-			for(Integer i:bucket.keySet()) {
+		boolean selected;
+		
+		for(int i = 0; i <E; i++) {
+			if(!bucket.containsKey(i)) {
+				list = new ArrayList<Integer>();
+				list.add(i);
+				bucket.put(i, list);
+				
+			}
+			else {
 				list = bucket.get(i);
-				for(int j = 0; j < list.size(); j++) {
-					int el = list.get(j);
-					for(Integer rev:reverseKeyset) {
-						if(rev == i)
-							break;
-						revList = bucket.get(rev);
-						insert = true;
-						for(int k = revList.size()-1; k >= 0; k--) {
-							if(conflicts[el][revList.get(k)] > 0 || conflicts[revList.get(k)][el] > 0) {
-								insert = false;
+				list.add(i);
+			}
+			for(int j = 0; j < E; j++) {
+				if(conflicts[i][j] > 0 || conflicts[j][i] > 0) {
+					if(!bucket.containsKey(j)) {
+						list = new ArrayList<Integer>();
+						list.add(i);
+						bucket.put(j, list);
+					}
+					else {
+						list = bucket.get(j);
+						selected = true;
+						for(Integer exam:list) {
+							if(conflicts[exam][i] == 0 && conflicts[i][exam] == 0 && i != exam) {
+								selected = false;
 								break;
 							}
 						}
-						if(insert) {
-							revList.add(el);
-							list.remove(j);
+						if(selected) {
+							list.add(i);
+						}
+					}
+				}
+			}
+		}
+		
+		return bucket.values().stream()
+				.sorted((s1, s2) -> Integer.compare(s2.size(), s1.size()))
+				.collect(Collectors.toList());
+		
+	}
+
+	private void solver(List<Integer> timeSlot, int bucketPosition, int position) {
+		if(bucketPosition >= buckets.size()) {
+			System.out.println("Catena finita");
+			return;
+		}
+		
+		boolean select = false;
+		
+		ArrayList<Integer> list = buckets.get(bucketPosition);
+		for(Integer i:list) {
+			if(isSelected[i] == 0) {
+				select = true;
+				for(Integer j: timeSlot) {
+					if(conflicts[j][i] > 0 || conflicts[i][j] > 0) {
+						select = false;
+						break;
+					}
+				}
+				if(select) {
+					timeSlot.add(i);
+					isSelected[i] = 1;
+					totExm--;
+					solver(timeSlot, bucketPosition+1, position+1);
+					break;
+				}
+			}
+			
+			
+		}
+		solver(timeSlot, bucketPosition + 1, position);
+	}
+	
+	private void solver() {
+		for(ArrayList<Integer> bucket:buckets) {
+			for(Integer exam:bucket) {
+				if(isSelected[exam] == 0) {
+					for(ArrayList<Integer> timeslot:timeslots) {
+						boolean selected = true;
+						for(Integer e2:timeslot) {
+							if(conflicts[exam][e2] > 0 || conflicts[e2][exam] > 0) {
+								selected = false;
+								break;
+							}
+						}
+						if(selected) {
+							timeslot.add(exam);
+							isSelected[exam] = 1;
+							totExm--;
 							break;
 						}
 					}
 				}
 			}
 		}
-		List<Integer> keySet =  bucket.keySet().stream().collect(Collectors.toList());
-		for(int i = 0; i < keySet.size(); i++) {
-			if(bucket.get(keySet.get(i)).size() == 0)
-				bucket.remove(keySet.get(i));
-		}
-		return bucket;
 	}
-
+	
+	private void solver(int position, int minPos, int maxPos) {
+		if(position >= buckets.size()) {
+			return;
+		}
+		
+		ArrayList<Integer> list = buckets.get(position);
+		for(int i = minPos; i < maxPos; i++) {
+			ArrayList<Integer> t = timeslots.get(i);
+			for(Integer e1:list) {
+				if(isSelected[e1] == 0) {
+					boolean selected = true;
+					for(Integer e2:t) {
+						if(conflicts[e1][e2] > 0 || conflicts[e2][e1] > 0) {
+							selected = false;
+							break;
+						}
+					}
+					if(selected) {
+						t.add(e1);
+						isSelected[e1] = 1;
+						totExm--;
+						break;
+					}
+				}
+			}
+		}
+		solver(position+1, minPos, maxPos);
+	}
+	
 	public int[] getSolution() {
 		return solution;
+	}
+	
+	private void writeLog() throws IOException {
+		String fileName = "log.txt";
+		try(BufferedWriter r = new BufferedWriter(new FileWriter(fileName))) {
+			for(ArrayList<Integer> a:buckets) {
+				String s = "";
+				for(Integer I:a) {
+					s = s + I + " ";
+				}
+				s = s + "\n";
+				r.write(s);
+			}
+			
+			r.write(Integer.toString(buckets.get(0).size()));
+		}
 	}
 
 }
